9b20d91e1c0f8ef5b6322f5d9326d8f1a493faf5
Lenvin <lenvingonsalves@gmail.com>
2021-04-01 12:03:10 +0530

    feat(Head): Add new component

---
+++ components/Head/index.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+import NextHead from "next/head";
+
+export const Head = () => (
+    <NextHead>
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+      <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans&display=swap" rel="stylesheet"></link>
+    </NextHead>
+);
