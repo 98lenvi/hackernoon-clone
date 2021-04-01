a1071830e7d5ecc918988c261f9ae61314e697d3
Lenvin <lenvingonsalves@gmail.com>
2021-04-01 12:01:15 +0530

    feat(assets): Add assets url

---
+++ assets/index.ts
@@ -0,0 +1,12 @@
+
+export const ThumbsDown= 'https://hackernoon.com/emojis/thumbs-down.png'
+export const Money= 'https://hackernoon.com/emojis/money.png'
+export const Light= 'https://hackernoon.com/emojis/light.png'
+export const Heart= 'https://hackernoon.com/emojis/heart.png'
+export const Twitter= "https://hackernoon.com/social-icons/twitter-new.png"
+export const Facebook="https://hackernoon.com/social-icons/facebook-new.png"
+export const LinkedIn="https://hackernoon.com/social-icons/linkedin-new.png"
+export const Email="https://hackernoon.com/social-icons/email-new.png"
+export const BrandLogo="https://hackernoon.com/hn-logo.png"
+export const BrandIcon="https://hackernoon.com/hn-icon.png"
+export const Search="https://hackernoon.com/search-new.png"
