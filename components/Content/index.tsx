835c4e995242ac1a9fad0f9cced2a8adb5d485cd
Lenvin <lenvingonsalves@gmail.com>
2021-04-01 12:02:51 +0530

    feat(Content): Add new component

---
+++ components/Content/index.tsx
@@ -0,0 +1,62 @@
+import * as React from "react";
+import Interweave, { Node } from 'interweave';
+import styled from "styled-components";
+import { polyfill } from 'interweave-ssr';
+import InlineReactions from '../InlineReactions';
+import Gist from 'react-gist';
+
+export const ContentParagraph = styled.p`
+    @media (max-width: ${(_) => _.theme.breakpoints.lg}) {
+        margin-top: -1rem !important;
+        margin-bottom: -1rem !important;
+    }
+`
+
+function Content({ markup }) {
+    polyfill();
+
+    const transform = (node: HTMLElement, children: Node[]): React.ReactNode => {
+        if (node.tagName === 'div' && node.getAttribute('class') === 'paragraph') {
+            let [mouseEnter, setMouseEnter] = React.useState(false);
+            const overallScore = 0.4;
+            const sentimentScoreArray = [
+                4,
+                5,
+                8,
+                9
+            ]
+
+            return (
+                <>
+                    <ContentParagraph onMouseEnter={() => setMouseEnter(true)} onMouseLeave={() => setMouseEnter(false)} >{children}</ContentParagraph>
+                    <InlineReactions overallScore={overallScore} sentimentScoreArray={sentimentScoreArray} mouseEnter={mouseEnter} />
+                </>
+            );
+        } else if (node.tagName === 'div' && node.getAttribute('class') === 'gist-container') {
+            let gistId = '';
+            node.childNodes.forEach(
+                childNode => {
+                    //@ts-ignore
+                    const id: string = childNode.getAttribute('id');
+                    if (id)
+                        gistId = id.replace('gist-', '');
+                }
+            )
+            return (
+                <div className='gist-container'>
+                    <Gist id={gistId} />
+                </div>
+            );
+        }
+    }
+    return (
+        <Interweave
+            content={markup}
+            transform={transform}
+            tagName={'fragment'}
+            allowElements={true}
+        />
+    )
+}
+
+export default Content;
