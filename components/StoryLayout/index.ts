7dcaf7d776db75ba3d6f1bbb00dc34b606a07ca0
Lenvin <lenvingonsalves@gmail.com>
2021-04-01 15:18:20 +0530

    fix(StoryLayout): fix width during load in mobile

--- components/StoryLayout/index.ts
+++ components/StoryLayout/index.ts
@@ -7,7 +7,7 @@ const StoryLayout = styled.div`
     gap: 2rem 25px;
     margin-bottom: 4rem;
     @media (max-width: ${(_) => _.theme.breakpoints.lg}) {
-      grid-template-columns: 1fr;
+      grid-template-columns: 100%;
       gap: 2rem 0px;
     }
     font-size: 2rem;
