7b81ad1c6c155e065b0199af57b1387d3c67931d
Lenvin <lenvingonsalves@gmail.com>
2021-04-01 13:32:10 +0530

    feat(StoryLayout): Add new component

---
+++ components/StoryLayout/index.ts
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+import { grid14, grid23 } from '../../theme/helpers';
+
+const StoryLayout = styled.div`
+    display: grid;
+    grid-template-columns: 1fr 3fr 1fr;
+    gap: 2rem 25px;
+    margin-bottom: 4rem;
+    @media (max-width: ${(_) => _.theme.breakpoints.lg}) {
+      grid-template-columns: 1fr;
+      gap: 2rem 0px;
+    }
+    font-size: 2rem;
+    h1 {
+      ${grid14}
+      text-align: center;
+    }
+    h2, h3, .youtube-container, .gist-container, p {
+      ${grid23}
+    }
+    blockquote, .code-container {
+      ${grid14}
+    }
+`;
+
+export default StoryLayout;
