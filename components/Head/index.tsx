fb3b3b2ab9502e45c83ac60da34b474433acfa5a
Lenvin <lenvingonsalves@gmail.com>
2021-04-01 13:59:01 +0530

    fix(Head): Add support for children

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import NextHead from "next/head";
 
-export const Head = () => (
+export const Head = ({children}) => (
     <NextHead>
       <link rel="preconnect" href="https://fonts.gstatic.com" />
       <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=IBM+Plex+Sans&display=swap" rel="stylesheet"></link>
+      {children}
     </NextHead>
 );
+
+export default Head;
